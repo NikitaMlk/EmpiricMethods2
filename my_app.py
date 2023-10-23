3171c155dee396d327be0e3697e86777d4e00077
NikitaMlk <125889319+NikitaMlk@users.noreply.github.com>
2023-10-23 16:36:12 +0300

    my_app.py

--- my_app.py
+++ my_app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.write('Hello World')
+st.title("Data Visualization App")
+    data = load_data()
+
+    st.sidebar.header("Data Filters")
+    min_rows = st.sidebar.number_input("Minimum Number of Rows", min_value=0, value=0)
+    selected_features = st.sidebar.multiselect("Select Features for Visualization", data.columns)
+
+    filtered_data = data.head(min_rows)  # Filter by the number of rows
+
+    st.sidebar.header("Visualization Settings")
+    color_type = st.sidebar.selectbox("Color Type", ['b', 'g', 'r', 'c', 'm', 'y', 'k'])
+    line_type = st.sidebar.selectbox("Line Type", ['solid', 'dotted', 'dashed'])
+
+    st.subheader("Feature Importance")
+    feature_importance(data, selected_features)
+
+    st.subheader("Numeric Feature Distribution")
+    plot_numeric_distribution(data, selected_features, color_type, line_type)
