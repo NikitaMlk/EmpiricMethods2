28d4c98df1d1e3ec67e1eb8fc5e85827dbb93d83
NikitaMlk <125889319+NikitaMlk@users.noreply.github.com>
2023-10-23 16:52:43 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -13,6 +13,77 @@ def load_data():
         st.error(f"Error loading data: {str(e)}")
         return None
 
+# Function to calculate feature importance
+def feature_importance(data, target_col):
+    if target_col not in data.columns:
+        st.warning(f"{target_col} not found in the dataset.")
+        return
+
+    # Split the data into features and target
+    X = data.drop(columns=[target_col])
+    y = data[target_col]
+
+    # Encode the categorical labels to numeric
+    label_encoder = LabelEncoder()
+    y_encoded = label_encoder.fit_transform(y)
+
+    # Impute missing values
+    imputer = SimpleImputer(strategy='mean')
+    X_imputed = imputer.fit_transform(X)
+
+    # Fit a random forest classifier to determine feature importance
+    clf = RandomForestClassifier()
+    clf.fit(X_imputed, y_encoded)
+
+    # Get feature importances
+    feature_importances = clf.feature_importances_
+
+    # Create a DataFrame to store feature names and their importance scores
+    importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': feature_importances})
+
+    # Sort features by importance
+    importance_df = importance_df.sort_values(by='Importance', ascending=False)
+
+    # Plot feature importance using Plotly
+    fig = px.bar(importance_df, x='Importance', y='Feature', orientation='h', title='Feature Importance')
+    st.plotly_chart(fig)
+
+# Function to plot numeric distribution
+def plot_numeric_distribution(data, selected_features, color_type, line_type):
+    if not selected_features:
+        st.warning("Please select numeric features for visualization.")
+        return
+
+    numeric_features = data.select_dtypes(include=['float64', 'int64'])
+
+    # Create subplots
+    fig, axes = plt.subplots(1, len(selected_features), figsize=(15, 5))
+
+    # Set line style
+    line_styles = ['solid', 'dotted', 'dashed']
+    line_style = line_styles[0]  # Default to solid line
+    if line_type == 'dotted':
+        line_style = 'dotted'
+    elif line_type == 'dashed':
+        line_style = 'dashed'
+
+    # Set color
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    color = colors[0]  # Default to blue
+    if color_type in colors:
+        color = color_type
+
+    # Plot histograms for each selected numeric feature
+    for i, col in enumerate(selected_features):
+        ax = axes[i]
+        sns.histplot(data[col], kde=True, ax=ax, color=color, linestyle=line_style)
+        ax.set_title(f'Distribution of {col}')
+        ax.set_xlabel(col)
+        ax.set_ylabel('Frequency')
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
 st.title("Data Visualization App")
 data = load_data()
 
